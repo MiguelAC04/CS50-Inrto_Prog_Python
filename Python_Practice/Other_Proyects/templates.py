c4e11aa93a5a1ddcb0a4ce89e1a4fa4d921d1735
MiguelAC04 <120741257+MiguelAC04@users.noreply.github.com>
2024-02-25 17:39:49 +0000

    Sun, Feb 25, 2024, 12:39 PM -05:00

--- Python_Practice/Other_Proyects/templates.py
+++ Python_Practice/Other_Proyects/templates.py
@@ -6,8 +6,9 @@ of the Template sting class
 '''
 
 def main() ->  None:
-    templ_1 = Template('$person is $yzears old')
-    print(templ_1.substitute(person='He', years=13))
-    
+    templ_1 = Template('$person is $years old $Noun')
+    mapp = {'Noun' : 10, 'person' : 'She'}
+    print(templ_1.substitute(mapp, person='He', years=13))
+
 
 main()
