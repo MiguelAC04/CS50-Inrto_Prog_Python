f1f69c373f89f0196c9f27e9920b64213a75103c
MiguelAC04 <120741257+MiguelAC04@users.noreply.github.com>
2024-02-25 15:54:35 +0000

    Sun, Feb 25, 2024, 10:54 AM -05:00

--- e.py
+++ e.py
@@ -46,6 +46,19 @@ def main() -> None:
         max = 133
         p_ints = [1,]
         euler(first_call=True, k=_k, n=_k-1)
+    else:
+         a = b = c = d = 0
+         for _ in range(26, 133):
+              a +=1
+              for _ in range(83, 133):
+                   b += 1
+                   for _ in range(109, 133):
+                        c += 1
+                        for _ in range(133):
+                             d += 1
+                             check_Euler(4, (a,b,c,d))
+
+
 
 
 def check_Euler(k: int, p_ints:list) -> None:
@@ -57,6 +70,7 @@ def check_Euler(k: int, p_ints:list) -> None:
         result = pow(sum_ints, (1/k))
         if int(result) == round(result, 10):
             counter_ex.append({f'Exponent: {k}':f'{p_ints} -> {int(result)}'})
+            exit('Terminated: '+ p_ints + result)
         print(p_ints, result)
 
 
