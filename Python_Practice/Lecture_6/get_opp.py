5fcf82ccf4df1f8760b6214ccd5d0a24f67aa179
MiguelAC04 <120741257+MiguelAC04@users.noreply.github.com>
2024-02-25 19:06:04 +0000

    Sun, Feb 25, 2024, 2:06 PM -05:00

--- Python_Practice/Lecture_6/get_opp.py
+++ Python_Practice/Lecture_6/get_opp.py
@@ -17,7 +17,7 @@ def check_decimal(num):
 
 def check_values(action, operator):
     values = action.split(operator[0])
-    for x in list(map(check_decimal, values)):
+    for x in map(check_decimal, values):
         print(x)
 
     # if x:=):
