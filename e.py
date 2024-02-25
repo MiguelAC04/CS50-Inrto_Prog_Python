4d013c208d9e9437068190ad272e72cb0a7e3bba
MiguelAC04 <120741257+MiguelAC04@users.noreply.github.com>
2024-02-25 16:03:58 +0000

    Sun, Feb 25, 2024, 11:03 AM -05:00

--- e.py
+++ e.py
@@ -9,7 +9,7 @@ Se realizará la demostración mediante el cálculo de un contra
 ejemplo para algún k menor a 6, dónde para un n menor a k se
 cumple la igualdad.
 '''
-
+math.
 
 
 def main() -> None:
