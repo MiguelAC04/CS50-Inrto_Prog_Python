9896a0741171fde79cfc3e009049e4dcf125b603
MiguelAC04 <120741257+MiguelAC04@users.noreply.github.com>
2024-02-25 18:55:02 +0000

    Sun, Feb 25, 2024, 1:55 PM -05:00

--- Python_Practice/Lecture_6/get_opp.py
+++ Python_Practice/Lecture_6/get_opp.py
@@ -1,7 +1,22 @@
 def main() -> None:
     operation = input('Opperation>> ')
     operators = ('+', '-', '*', '/')
-    op_parts = opperation.split()
+    op_parts = operation.split()
+    intersection = lambda l1, l2: list(set(l1).intersection(l2))
     for part in op_parts:
-        focus = list(set(op_parts).)
-        if
+        if (op := intersection(part, operators)):
+            check_values(part, op)
+
+
+def check_decimal(num):
+    return num.replace('.', '', 1).isdigit()
+
+
+def check_values(action, operator):
+    values = action.split(operator)
+    if x:=map(check_decimal, values):
+        pass
+    print(x)
+    # return values.replace('.', 1)
+if __name__ == "__main__":
+    main()
