78c8a6a3a43069476d87372ecb46cea980f77dd9
MiguelAC04 <120741257+MiguelAC04@users.noreply.github.com>
2024-02-25 17:42:47 +0000

    Sun, Feb 25, 2024, 12:42 PM -05:00

--- Python_Practice/Other_Proyects/templates.py
+++ Python_Practice/Other_Proyects/templates.py
@@ -9,6 +9,8 @@ def main() ->  None:
     templ_1 = Template('$person is $years old $Noun')
     mapp = {'Noun' : 10, 'person' : 'She'}
     print(templ_1.substitute(mapp, person='He', years=13))
+    templ_2 = Template('$Person plays $piano')
+    print(templ_2.safe_substitute())
 
 
 main()
