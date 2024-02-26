6307faed8dc19ac7c7baa7d84bc44bfb8d70d37a
MiguelAC04 <120741257+MiguelAC04@users.noreply.github.com>
2024-02-26 01:50:37 +0000

    Sun, Feb 25, 2024, 8:50 PM -05:00

--- Python_Practice/Lecture_6/get_opp.py
+++ Python_Practice/Lecture_6/get_opp.py
@@ -1,6 +1,6 @@
 def main() -> None:
     operation = input('Opperation>> ')
-    operators = ('+', '-', '*', '/', 'x')
+
     op_parts = operation.split()
 
     intersection = lambda l1, l2: list(set(l1).intersection(l2))
@@ -16,18 +16,20 @@ def check_decimal(num):
 
 
 def check_values(action: str, operator: list[str]) -> bool:
-    def get_nums() -> list[str]:
-        if operator not in action:
-            return
-    values = list()
-    for op in operator:
-        values.append(action.split(op))
-    print(values)
-    return
-    for valid_n in map(check_decimal, values):
-        if not valid_n:
-            return
-    print(eval('43xe'))
+    operators = ('+', '-', '*', '/', 'x')
+    # def get_nums() -> list[str]:
+    #     if operator not in action:
+    #         return
+    # values = list()
+    # for op in operator:
+    #     values.append(action.split(op))
+    # print(values)
+    print(action.split(operators))
+    # return
+    # for valid_n in map(check_decimal, values):
+    #     if not valid_n:s
+    #         return
+    # print(eval('43xe'))
 
 
 if __name__ == "__main__":
