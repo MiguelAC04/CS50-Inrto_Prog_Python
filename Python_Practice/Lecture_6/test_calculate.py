39225afb6f44b2a9e5b62dcc7634aa9ee580cd5a
MiguelAC04 <120741257+MiguelAC04@users.noreply.github.com>
2024-02-26 02:57:28 +0000

    Sun, Feb 25, 2024, 9:57 PM -05:00

--- Python_Practice/Lecture_6/test_calculate.py
+++ Python_Practice/Lecture_6/test_calculate.py
@@ -2,8 +2,12 @@ import calculate
 
 
 def main() -> None:
-    oper_1 = '13*13+23/2'
-    
+    oper_1 = '13x13+23/2'
+    res_1 eval(oper_1.replace('x', '*'))
+    try:
+        assert res_1 == calculate.main(oper_1)
+    except AssertionError:
+        print('Wrong answer for function')
 
 
 if __name__ == "__main__":
