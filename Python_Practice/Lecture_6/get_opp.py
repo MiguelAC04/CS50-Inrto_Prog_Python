f053158636a4c5083ae7309f75a6f05f4917df4a
MiguelAC04 <120741257+MiguelAC04@users.noreply.github.com>
2024-02-26 01:54:13 +0000

    Sun, Feb 25, 2024, 8:54 PM -05:00

--- Python_Practice/Lecture_6/get_opp.py
+++ Python_Practice/Lecture_6/get_opp.py
@@ -16,20 +16,13 @@ def check_decimal(num):
 
 
 def check_values(action: str, operator: list[str]) -> bool:
-    operators = ('+', '-', '*', '/', 'x')
-    # def get_nums() -> list[str]:
-    #     if operator not in action:
-    #         return
-    # values = list()
-    # for op in operator:
-    #     values.append(action.split(op))
-    # print(values)
-    print(action.split(operators))
-    # return
-    # for valid_n in map(check_decimal, values):
-    #     if not valid_n:s
-    #         return
-    # print(eval('43xe'))
+    for op in operator:
+        action = ' '.join(action.split(op))
+    values = action.split()
+    for valid_n in map(check_decimal, values):
+        if not valid_n:s
+            return
+    print(eval('43xe'))
 
 
 if __name__ == "__main__":
