91f14acc262c2d04d4c259acb9f840a30d7cfb74
MiguelAC04 <120741257+MiguelAC04@users.noreply.github.com>
2024-02-26 03:00:59 +0000

    Sun, Feb 25, 2024, 10:00 PM -05:00

--- Python_Practice/Lecture_6/test_calculate.py
+++ Python_Practice/Lecture_6/test_calculate.py
@@ -1,11 +1,11 @@
-import calculate
+from calculate import main as calc_expr
 
 
 def main() -> None:
     oper_1 = '13x13+23/2'
     print(oper_1, res_1 := eval(oper_1.replace('x', '*')))
     try:
-        assert res_1 == calculate.main(oper_1)
+        assert res_1 == calc_expr(oper_1)
     except AssertionError:
         print('Wrong answer for function')
 
