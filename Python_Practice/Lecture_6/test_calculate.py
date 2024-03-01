af5d75a90be17d2e6d43ed36851e22229789f75c
MiguelAC04 <120741257+MiguelAC04@users.noreply.github.com>
2024-03-01 21:45:38 +0000

    Fri, Mar 1, 2024, 4:45 PM -05:00

--- Python_Practice/Lecture_6/test_calculate.py
+++ Python_Practice/Lecture_6/test_calculate.py
@@ -5,9 +5,10 @@ def main() -> None:
     oper_1 = '13x13+23/2'
     print(oper_1, res_1 := eval(oper_1.replace('x', '*')))
     try:
-        assert res_1 == (r:=calc_expr(oper_1))[1]
-    except AssertionError:
-        print(f'Wrong answer for function: {res_1} vs {r}')
+        assert res_1 == (r:=calc_expr(oper_1))[1], f'{res_1} not = to {r}'
+    except AssertionError as err:
+        print('\t', err)
+        raise
 
 
 if __name__ == "__main__":
