05687d52bdc71e2c162bf93df5298a7b5f8de904
MiguelAC04 <120741257+MiguelAC04@users.noreply.github.com>
2024-03-01 22:46:35 +0000

    Fri, Mar 1, 2024, 5:46 PM -05:00

--- Python_Practice/Lecture_6/test_calculate.py
+++ Python_Practice/Lecture_6/test_calculate.py
@@ -4,31 +4,34 @@ from calculate import main as calc_expr
 def main() -> None:
     oper_1 = '13x13+23/2'
     print(oper_1, res_1 := eval(oper_1.replace('x', '*')))
+    return oper_1
 
-    def value_test()-> None:
+def value_test()-> None:
+    try:
+        assert res_1+1 == (r:=calc_expr(oper_1))[1], f'{res_1} not = to {r}'
+    except AssertionError as err:
+        print('\t',  err)
+
+def exception_test() -> None:
+    def assert_raises(err, func, *args) -> None:
         try:
-            assert res_1+1 == (r:=calc_expr(oper_1))[1], f'{res_1} not = to {r}'
-        except AssertionError as err:
-            print('\t',  err)
-
-    def exception_test() -> None:
-
-        def assert_raises(err, func, *args) -> None:
-            try:
-                func(args)
-            except err:
-                print('Error raised succesfuly')
-                return True
-            except Exception:
-                return False
-
-        try:
-            assert assert_raises(   err:=SyntaxError,
-                                    calc_expr,
-                                    'NOT AN OPERATION'
-                                ), f'Not expected {err} raised'
+            func(args)
+        except err:
+            print('Error raised succesfuly')
+            return True
+        except Exception:
+            return False
+    try:
+        assert assert_raises(err:=SyntaxError,
+                                calc_expr,
+                                'NOT AN OPERATION'
+                            ), f'Not expected {err} raised'
         except AssertionError as unexpected_err:
             print(unexpected_err)
+
+
+
+
 
     value_test()
     exception_test()
