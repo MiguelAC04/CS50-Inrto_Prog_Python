976eb262d0ea277ab9329783cd46039c616e4aa0
MiguelAC04 <120741257+MiguelAC04@users.noreply.github.com>
2024-03-01 23:46:34 +0000

    Fri, Mar 1, 2024, 6:46 PM -05:00

--- Python_Practice/Lecture_6/test2_calculate.py
+++ Python_Practice/Lecture_6/test2_calculate.py
@@ -6,22 +6,24 @@ print(oper_1, res_1 := eval(oper_1.replace('x', '*')))
 
 
 def test_values():
-    assert res_1+1 == (r:=calc_expr(oper_1))[1], f'{res_1} not equal to {r}'
-
+    assert res_1 == (r:=calc_expr(oper_1))[1], f'{res_1} not equal to {r}'
 
 
 def test_operator_exception():
     test, msg = raises(SyntaxError, calc_expr, '90¨¨30__12')
     assert test, msg
 
+
 def test_value_exeption():
     test, msg = raises(ValueError, calc_expr, '3xa+2x+2')
     assert test, msg
 
-def test_operator_combination():
-    assert not raises(calc_expr, '12/+1+3*-1')
 
-def raises(expected_err = None, func, *args):
+def test_operator_combination():
+    assert not raises(None, calc_expr, '12/+1+3*-1')
+
+
+def raises(expected_err, func, *args):
     if expected_err:
         try:
             func(*args)
